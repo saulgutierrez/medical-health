3fbcdfa25fe55b3f59cf471e346addab70e90803
Saúl Gutiérrez <sauladai@hotmail.com>
2023-07-31 23:20:05 -0600

    Finish responsive design

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+/* Funcion para activar el menú responsive */
+function toggleMenu() {
+    var menuToggle = document.querySelector('.menuToggle');
+    var navbar = document.querySelector('.nav');
+    menuToggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+}
